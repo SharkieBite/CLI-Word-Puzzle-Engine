35540dd0c144eb673978d495b52912d88c3c5c1d
axell8560 <axell8560@gmail.com>
2025-11-26 11:20:34 -0500

    Refactor checkLetter function and main logic

--- checkletter.c
+++ checkletter.c
@@ -6,59 +6,75 @@
 #include "checkletter.h"
 
 int main(int argc, char *input[]) {
-    if (argc == 2 && strcmp(input[1], "--help") == 0) {
+    char *answer = "hello";
+    char *guess;
+    int lives = 5;
+    int *attempts = &lives;
+    int level = 5;
+
+    if (argc >= 2 && strcmp(input[1], "--help") == 0) {
         help();
         return 0;
     }
-    checkLetter(6);
+    puts("Enter guess");
+    while (1) {
+        if (scanf("%s", guess) != 1) {
+            printf("Answer was \033[32m%s\033[0m\n", answer);
+            return 0;
+        }
+        // to_lower(guess);
+        checkLetter(guess, answer, attempts, level);
+        if (*attempts == 0) {
+            return 0;
+        }
+    }
     return 0;
 }
 
-int checkLetter(int attempts) {
-    char answer[] = "hello";
-    char guess[100];
-    for (attempts; attempts > 0; attempts--) {
-        scanf(" %s", guess);
-        if (strlen(guess) == strlen(answer)) {
-            for (int i=0; i< sizeof(answer); i++) {
-                if (strchr(answer, guess[i]) != NULL) {
-                    if (guess[i] == answer[i]) {
-                        printf("\033[32m%c\033[0m", guess[i]); //green
-                    }
-                    else {
-                        printf("\033[33m%c\033[0m", guess[i]); //yellow
-                    }
-                }
-                else {
-                    printf("\033[31m%c\033[0m", guess[i]); //red
-                }
+int checkLetter(char *guess, char *answer, int *attempts, int level) { //Checks if letters match
+    int checker = 1;
+    // printf("%s\n", answer);
+    // if (strlen(guess) == level) {
+    *attempts -= 1;
+    for (int i=0; i< level; i++) {
+        if (strchr(answer, guess[i]) != NULL) {
+            if (guess[i] == answer[i]) {
+                printf("\033[32m%c\033[0m", guess[i]); //green
             }
-            puts("");
-            if (strcmp(guess, answer) == 0) {
-                puts("You win!");
-                return 0;
+            else {
+                printf("\033[33m%c\033[0m", guess[i]); //yellow
+                checker = 0;
             }
         }
         else {
-        puts("Invalid word length");
-        attempts ++;
+            printf("\033[31m%c\033[0m", guess[i]); //red
+            checker = 0;
         }
+        // printf(" guess: %c, answer: %c\n", guess[i], answer[i]);
     }
-
-    printf("\n");
-    puts("You are out of attempts");
-
-    return 0;
+    puts("");
+    if (checker == 1) {
+        puts("You win!");
+        exit(0);
+    }
+    else if (*attempts == 0) { 
+        puts("You are out of attempts");
+        printf("Answer was \033[32m%s\033[0m\n", answer);
+        return 0;
+    }
+    // }
 }
 
 int help() {
     puts("lettercheck v1.0.0");
     puts("Checks if letters in guess are in right spot");
-    puts("Usage: letterCheck(attempts)\n");
-    puts("Attempts give number of guesses user has");
-    puts("If <attempts> isn't given, default value is 6\n");
-    puts("<attempts> \t 1-10 \tset attempts to <attempts>");
+    puts("Usage: letterCheck(guess, answer, attempts, level)\n");
+    puts("<guess> \t\tchecks users guess");
+    puts("<answer> \t\tcompares letters in guess to answer");
+    puts("<attempts> \t 1-10 \tgives number of current attempts user has");
+    puts("<level> \t 5-8 \tchecks first <level> letters in answer");
     puts("--help \t\t\tdisplay this help and exit\n");
-    puts("Examples:\nletterCheck(6) \t\tGives user 6 guesses");
+    puts("Examples:\nletterCheck(hello, hello, 1, 5)\t\tTells user they win.");
+    puts("letterCheck(fraud, hello, 1, 5)\t\tTells user they lose.");
     return 0;
 }
