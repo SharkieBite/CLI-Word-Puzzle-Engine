e992e89f95226707cdb51d2d052b9ef3a283c9d7
Michael <148469567+SharkieBite@users.noreply.github.com>
2025-12-02 13:34:12 -0500

    [+] Added comments to .h file

--- playWords.h
+++ playWords.h
@@ -1,8 +1,10 @@
+//Defines constant variable that will never change throughout the program
 #include <stdbool.h>
 #define MAX_GUESSES 6        
 #define MAX_WORD_LENGTH 6    
 #define MAX_LINE_LENGTH 16
 
+//Declares the functions as a prototypes
 char *chooseWord(int number);
 bool checkWord(int level, char *guess);
 int checkLetter(char *guess, char *answer, int *attempts, int level);
